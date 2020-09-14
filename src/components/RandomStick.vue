e6524916fd86a4ea242c3b1f11ef92bf6b7ac095
PleasantGoat84 <PleasantGoat84@gmail.com>
2020-09-14 23:03:50 +0800

    random seems to be working

---
+++ src/components/RandomStick.vue
@@ -0,0 +1,44 @@
+<template>
+  <div
+    class="stick"
+    :style="{
+      transform: `rotateZ(${option.k}rad)`,
+      left: `${6 + 5.5 * option.x}vw`
+    }"
+  />
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+
+export interface StickOption {
+  x: number;
+  k: number;
+}
+
+@Component
+export default class RandomStick extends Vue {
+  @Prop() private option!: StickOption;
+}
+</script>
+
+<style lang="scss" scoped>
+.stick {
+  $stick-width: 50vh;
+  $stick-height: $stick-width / 13;
+
+  width: $stick-width;
+  height: $stick-height;
+
+  background: wheat;
+  border: $stick-height / 5 solid burlywood;
+
+  border-radius: $stick-height / 2;
+
+  // most positioning handled in :style
+  transform-origin: center left;
+
+  position: absolute;
+  bottom: 5vh;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,60 @@
 <template>
-  <div class="home">
-    <img src="@/assets/basket.png" />
+  <div class="home d-flex align-center justify-center fill-height">
+    <div>
+      <div class="basket-container">
+        <RandomStick
+          v-for="(option, i) in stickOptions"
+          :key="i"
+          :option="option"
+        />
+        <v-img src="@/assets/basket.png" width="15vw" class="mx-auto basket" />
+      </div>
+
+      <v-btn color="primary" class="mt-10">抽籤</v-btn>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+import RandomStick, { StickOption } from "@/components/RandomStick.vue"; // @ is an alias to /src
 
 @Component({
   components: {
-    HelloWorld
+    RandomStick
   }
 })
-export default class Home extends Vue {}
+export default class Home extends Vue {
+  readonly basketHeight = window.innerWidth * 0.13;
+  readonly basketWidth = window.innerWidth * 0.1;
+
+  private stickOptions: Array<StickOption> = [];
+
+  created() {
+    for (let i = 0; i < 18; i++) {
+      const x = Math.random();
+      const exactX = x * this.basketWidth;
+
+      const minRotate = Math.atan(
+        this.basketHeight / (this.basketWidth - exactX)
+      );
+      const maxRotate = Math.PI - Math.atan(this.basketHeight / exactX);
+      const k = -(Math.random() * (maxRotate - minRotate) + minRotate);
+
+      console.debug(x, minRotate, maxRotate, k);
+
+      this.stickOptions.push({ x, k });
+    }
+  }
+}
 </script>
+
+<style lang="scss" scoped>
+.basket-container {
+  position: relative;
+
+  .basket {
+    margin-top: 30vh;
+  }
+}
+</style>
